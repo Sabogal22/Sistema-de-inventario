ccee3247545e37e02aa486e25f7dd9401a6454e7
Sabogal22 <nsabogalgaitan@gmail.com>
2025-03-03 21:49:00 -0500

    Configuracion de CORS

--- backend/warehouse_system/settings.py
+++ backend/warehouse_system/settings.py
@@ -137,3 +137,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.User'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # URL del frontend en React
+]
