db878f56b8b3a8efdabedb1fa68f2735db1c081a
Sabogal22 <nsabogalgaitan@gmail.com>
2025-03-22 20:34:12 -0500

    implemenation de la vista de pruductos, codigo endentado

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import Navbar from "./Navbar"; // Se renderiza solo cuando el usuario está autenticado.
+import Navbar from "./Navbar";
 
 const PrivateRoute = () => {
     const token = localStorage.getItem("access_token");
@@ -11,8 +11,8 @@ const PrivateRoute = () => {
 
     return (
         <>
-            <Navbar /> {/* Se renderiza solo si el usuario está autenticado */}
-            <Outlet /> {/* Renderiza las rutas hijas como Dashboard y Products */}
+            <Navbar />
+            <Outlet />
         </>
     );
 };
