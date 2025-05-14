e2414d6cea2a4f6598d05cd8b35de56745682b3a
Neythan Sabogal Gaitan <110112341+Sabogal22@users.noreply.github.com>
2025-05-14 10:39:09 -0500

    Update settings.py

--- backend/inventario/settings.py
+++ backend/inventario/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTH_USER_MODEL = 'api.User'
